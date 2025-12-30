5f7d3ff9767b240b0c65e4e69dbc9854258893b3
rze <rze.entwicklung+github@gmail.com>
2025-12-30 17:39:20 +0100

    update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/page-creator/",
+  base: "/localama/",
   plugins: [
     vue(),
     VitePWA({
